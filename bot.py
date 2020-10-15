457395689035e1f34da1beaceddaaa3b113e1817
newfirstname <newfirstnamenewlastname1@gmail.com>
2020-10-15 13:09:07 +0330

    dev

--- bot.py
+++ bot.py
@@ -3,20 +3,17 @@ from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.sync import events
 from telethon import functions, types
 import telethon
-import asyncio
-from bot_functions import *
 
 bot_name = 'poster-bot'
 API_ID = 1759413
 API_HASH = '609e3756b5a95466c9422ab57eea37bb'
-BOT_TOKEN = '345211056:AAGYfHCW8GRliU1hF3zWrGVHOH16PbwKiOg'
+BOT_TOKEN = '1345211056:AAFR3DP5XNfHtQWosz2xZkqV9kkpcSJK9fc'
 
-bot = TelegramClient(bot_name, API_ID, API_HASH)
-
-bot.start(bot_token=BOT_TOKEN)
+bot = TelegramClient(bot_name, API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
 @bot.on(events.NewMessage)
 async def bot_new_message_handler(event):
+  print('new message')
   if(event.raw_text.startswith('/')):
     command = event.raw_text.split('/')[1]
     
@@ -26,10 +23,6 @@ async def bot_new_message_handler(event):
 
 async def botCommandRecieved(event, command):
   if command == 'start':
-    chat = await event.get_chat()
-
-    print('user id: ', chatId)
-
     await event.respond('you will get new messages here')
 
   # set destination channel
